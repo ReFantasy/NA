1eadd2860b24a83dca3a5c94d87f17385c26543b
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-01-15 11:49:23 +0800

    update

--- src/NumercialAnalysis/NLE/bisection.py
+++ src/NumercialAnalysis/NLE/bisection.py
@@ -1,20 +1,29 @@
-def bisection(I,func, tol=1e-5):
+def bisection(I,func, tol=1e-5, max_iter=None):
+    cur_iter = 0
+
     a, b = I
     x = (a + b) / 2
-    value = func(x)
+    val = func(x)
 
-    if value == 0:
+    if val == 0:
         return x
 
     while True:
-        if func(a) * value < 0:
+        if func(a) * val < 0:
             b = x
         else:
             a = x
         x_n = (a + b) / 2
-        if abs(x_n - x) < tol:
+
+        val = func(x_n)
+        if (abs(x_n - x) < tol) or (val == 0):
             return x_n
+        
+        # Check for maximum iterations
+        cur_iter += 1
+        if max_iter is not None:
+            if cur_iter >= max_iter:
+                return x_n
         x = x_n
-        value = func(x)
 
 
