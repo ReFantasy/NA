3617269a52092d5c4e0a0b8938956f6bdb43b790
ReFantasy <309741309@qq.com>
2026-01-17 22:29:35 +0800

    update

--- src/NumercialAnalysis/NLE/newton.py
+++ src/NumercialAnalysis/NLE/newton.py
@@ -1,6 +1,6 @@
 import jax
 from functools import singledispatch
-from jax import grad, jacrev
+from jax import grad, jacrev, jacfwd
 import jax.numpy as jnp
 
 def newton(func, x0, tol):
@@ -14,21 +14,6 @@ def newton(func, x0, tol):
         xk_c = xk_n
         xk_n = xk_c - func(xk_c) / grad_f(xk_c)
     return xk_n
-
-# def newton_mp(func, x0, x1,  tol):
-#     """
-#     Multi-point Newton's method
-#     """
-#     def gx(func,x0, x1):
-#         return x1 - (x1-x0)/(func(x1)-func(x0)) * func(x1)
-#     xk_p = x0
-#     xk_c = x1
-#     xk_n = gx(func, xk_p, xk_c)
-#     while abs(xk_n - xk_c) > tol:
-#         xk_p = xk_c
-#         xk_c = xk_n
-#         xk_n = gx(func, xk_p, xk_c)
-#     return xk_n
 
 def newton_mp(func, x0, x1,  tol):
     """
@@ -58,7 +43,7 @@ def newtons(func, x0, tol):
         return jnp.array(func(x))
     x0 = jnp.array(x0)
 
-    grad_f = jacrev(jax_func)
+    grad_f = jax.jit(jacrev(jax_func))
     xk_c = x0
 
     H = grad_f(xk_c)
