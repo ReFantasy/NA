c9c2248e911375aa701bb6880eea8847fd0ce6e1
ReFantasy <309741309@qq.com>
2026-03-26 16:59:56 +0800

    update pass_by_pass jacobi

--- NumercialAnalysis/Eigen/jacobi.py
+++ NumercialAnalysis/Eigen/jacobi.py
@@ -31,10 +31,48 @@ def jacobi(A: jnp.array, tol: float = 1e-12):
         # 更新矩阵和特征向量
         Q = Q @ J
         A = J.T @ A @ J
+        # print(Q)
+        # print("------------------------------")
 
         # 检查非对角元素的平方和
         off_diag_sum = jnp.sum(jnp.square(A - jnp.diag(jnp.diag(A))))
         if off_diag_sum < jnp.abs(tol):
             break
+
+    return Q, jnp.diag(A)
+
+
+def passby_jacobi(A: jnp.array, tol: float = 1e-12, scale: float = 0.5):
+    # 检查输入矩阵是否为实对称矩阵
+    if not jnp.allclose(A, jnp.transpose(A)):
+        raise ValueError("Input matrix must be symmetric.")
+
+    scale = jnp.abs(scale)
+    scale = scale if scale < 1 else 0.5
+
+    # 创建单位矩阵
+    Q = jnp.eye(A.shape[0])
+
+    alpha = jnp.sum(jnp.square(A - jnp.diag(jnp.diag(A)))) / A.shape[0]
+
+    while alpha > tol:
+        for i in range(A.shape[0]):
+            for j in range(i + 1, A.shape[1]):
+                if jnp.abs(A[i, j]) > alpha:
+                    p, q = i, j
+                    # 计算旋转角度
+                    theta_k = 0.5 * jnp.arctan(2 * A[p, q] / (A[p, p] - A[q, q]))
+
+                    # 创建旋转矩阵
+                    c = jnp.cos(theta_k)
+                    s = jnp.sin(theta_k)
+                    J = jnp.eye(A.shape[0])
+                    J = J.at[p, p].set(c).at[q, q].set(c).at[p, q].set(-s).at[q, p].set(s)
+                    
+
+                    # 更新矩阵和特征向量
+                    Q = Q @ J
+                    A = J.T @ A @ J
+        alpha *= scale
 
     return Q, jnp.diag(A)
--- example/eigen.py
+++ example/eigen.py
@@ -1,17 +1,45 @@
 import NumercialAnalysis as na
 import jax.numpy as jnp
+import jax
+import time
 
+def compute_time():
+    # 生成一个随机的实对称矩阵
+    dims = 7
+    key = jax.random.PRNGKey(0)  
+    A = jax.random.normal(key, (dims, dims))
+    A = (A + A.T) / 2  
 
-if __name__ == "__main__":
-    A = jnp.array([[6, -2, 1], 
-                   [-2, 5, 3], 
-                   [1, 3, -4]])
-    
+    start_time = time.time()
     Q, lam = na.Eigen.jacobi(A, tol=1e-13)
+    end_time = time.time()
+    print(f"jacobi computation time: {end_time - start_time} seconds")
+
+    start_time = time.time()
+    Q, lam = na.Eigen.passby_jacobi(A, tol=1e-13)
+    end_time = time.time()
+    print(f"passby_jacobi computation time: {end_time - start_time} seconds")
+    
+if __name__ == "__main__":
+    # A = jnp.array([[6, -2, 1], 
+    #                [-2, 5, 3], 
+    #                [1, 3, -4]])
+    A = jnp.array([[2, -1, 0], 
+                   [-1, 2, -1], 
+                   [0, -1, 2]])
+  
+    
+    #Q, lam = na.Eigen.jacobi(A, tol=10e-5)
+    Q, lam = na.Eigen.passby_jacobi(A, tol=10e-5)
+   
     print("Eigenvalues:\n", lam)
     print("Eigenvectors (columns of Q):\n", Q)
+    
+    # print("Check error")
+    # for i in range(3):
+    #     print(Q[:,i] * lam[i] - A @ Q[:,i])
 
-    print("Check error")
-    for i in range(3):
-        print(Q[:,i] * lam[i] - A @ Q[:,i])
+    # compute_time()
+
+    
   
