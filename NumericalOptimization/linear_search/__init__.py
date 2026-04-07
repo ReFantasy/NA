cae3d61e6a7f0fc6403278056c69cefdedc7bf36
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-04-07 14:35:59 +0800

    update

--- NumericalOptimization/linear_search/__init__.py
+++ NumericalOptimization/linear_search/__init__.py
@@ -1,3 +1,4 @@
 from .golden import *
 from .fibonacci import fibonacci
 from .newton import *
+from .secant import *
---
+++ NumericalOptimization/linear_search/secant.py
@@ -0,0 +1,42 @@
+from jax import grad
+
+
+## 割线法
+def Secant(objfun: callable, a: float, b: float, epsilon: float):
+    # automatically compute the gradient of the objective function
+    gradfun = grad(objfun)
+
+    xk_1, xk = a, b
+    k = 0
+
+    while True:
+        k += 1
+        if abs(gradfun(xk)) <= epsilon:
+            xstar = xk
+            fstar = objfun(xk)
+            return xstar, fstar, k
+        else:
+            temp = xk
+            xk -= ((xk - xk_1) / (gradfun(xk) - gradfun(xk_1))) * gradfun(xk)  # 式（2-49）
+            xk_1 = temp
+
+
+if __name__ == "__main__":
+    import math
+    import jax
+
+    jax.config.update("jax_enable_x64", True)
+
+    @jax.jit
+    def objfun(x):
+        y = math.e ** (-x) + x**2
+        return y
+
+    # 输入
+    a = -4.0  # 初始区间左端点
+    b = 4.0  # 初始区间右端点
+    epsilon = 0.00001  # 容忍精度
+
+    # 割线法
+    xstar, fstar, k = Secant(objfun, a, b, epsilon)
+    print("Secant: ", xstar, fstar, k)
