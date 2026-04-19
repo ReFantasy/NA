6e24ee558a241da31438306e411c0dafb918e86b
ReFantasy <refantasy@ReFantasydeMac-mini.local>
2026-04-19 21:00:38 +0800

    update

--- NumericalOptimization/constrained_optimization/zoutendijk.py
+++ NumericalOptimization/constrained_optimization/zoutendijk.py
@@ -1,14 +1,17 @@
+from math import isclose
+
 import jax
 import jax.numpy as jnp
 from mpax import create_lp, r2HPDHG
-from jax.experimental import sparse
+# from jax.experimental import sparse
+from NumericalOptimization.utils import linear_search
 
 
-def feadesdir(fun, A, b, E, xk):
+def feadesdir(objfun, A, b, E, xk):
     """
     使用 JAX 和 MPAX 确定可行下降方向
     """
-    grad = jax.grad(fun)(xk)
+    grad = jax.grad(objfun)(xk)
 
     # 确定积极约束矩阵A1
     t = A @ xk
@@ -24,7 +27,7 @@ def feadesdir(fun, A, b, E, xk):
             E.shape[0],
         )  # 等式约束右侧值为零，满足 E d = 0
     else:
-        A_eq = sparse.BCOO.fromdense(jnp.zeros(shape=(1, A.shape[1])))  # 没有等式约束时，使用零矩阵
+        A_eq = jnp.zeros(shape=(1, A.shape[1]))  # 没有等式约束时，使用零矩阵
         b_eq = jnp.zeros(1)
 
     # 调用 MPAX 求解线性规划
@@ -37,19 +40,71 @@ def feadesdir(fun, A, b, E, xk):
         b=b_eq,
         G=-A_ub,
         h=b_ub,
-        l=-jnp.ones_like(xk),
-        u=jnp.ones_like(xk),
+        l=-1.0,#-jnp.ones_like(xk),
+        u=1.0,#jnp.ones_like(xk),
         use_sparse_matrix=False,
     )  # 定义解的范围 d \in [l, u]
-    solver = r2HPDHG(verbose=False)
+    solver = r2HPDHG(verbose=False, eps_abs=1e-4, eps_rel=1e-4,)
     result = solver.optimize(lp)
 
-    return result.primal_solution
+    return result.primal_solution, grad
 
+def linesearch(objfun, A, b, xk, d):
+    # 确定积极约束矩阵A1
+    t = A @ xk
+    active_mask = jnp.isclose(t, b)
+    # active_mask 取反，得到非积极约束矩阵A2
+    inactive_mask = ~active_mask
+    A2 = A[inactive_mask, :]
+    b2 = b[inactive_mask]
+    p = A2 @ d
+    q = b2 - A2 @ xk
 
-def zoutendijk(func, x0, max_iters=1000, tol=1e-6):
+    # 求步长上界
+    if jnp.all(p <= 0):
+        lambda_max = 10.0
+    else:
+        # 获取 p > 0 的正数部分的索引
+        mask_p_pos = p > 0
+        q_pos = q[mask_p_pos]
+        p_pos = p[mask_p_pos]
+        
+        lambda_max = jnp.min(q_pos / p_pos)
+        # 如果 lambda_max 出现异常值（例如没提取到任何项），提供一个回退
+        if jnp.isnan(lambda_max) or jnp.isinf(lambda_max):
+            lambda_max = 10.0
 
-    pass
+    # 设置一维搜索目标函数
+    # def phi(lam):
+    #     return objfun(xk + lam * d)
+    phi = lambda lam: objfun(xk + lam * d)
+    
+    
+    final_lambda, fstar, k = linear_search.golden(phi, a = 0.0, b = lambda_max, epsilon=0.01)
+    return final_lambda
+
+def zoutendijk(objfun, A, b, E, e, x0, max_iter=1000):
+    k= 0 
+    xk = x0
+
+    while True:
+        d, grad = feadesdir(objfun, A, b, E, xk)
+
+        if jnp.isclose(grad @ d, 0.0, atol=1e-3):
+            #print("Optimal solution found at iteration", k)
+            return xk, objfun(xk), k
+        
+        if k >= max_iter:
+            print("Maximum iterations reached")
+            return xk, objfun(xk), k
+
+        lam = linesearch(objfun, A, b, xk, d)
+        xk = xk + lam * d
+        k += 1
+        print(f"Iteration {k}: x = {xk}, f(x) = {objfun(xk)}, lambda = {lam}")
+
+        
+        
 
 
 if __name__ == "__main__":
@@ -59,11 +114,19 @@ if __name__ == "__main__":
         return x[0] ** 2 + x[1] ** 2 - 2 * x[0] - 4 * x[1] + 6.0
 
     A = jnp.array([[2.0, 1.0], [2.0, -1.0], [-1.0, 0.0], [0.0, -1.0]])
-    A = sparse.BCOO.fromdense(A)
+    #A = sparse.BCOO.fromdense(A)
     b = jnp.array([6.0, 0.0, 0.0, 0.0])
-    E = sparse.BCOO.fromdense(jnp.array([]))  # 没有等式约束
+    E = jnp.array([])  # 没有等式约束
     e = jnp.array([])  # 没有等式约束的右侧值
     x0 = jnp.array([1.0, 4.0])
+    #x0 = jnp.array([0.4483, 4.0112])
 
-    d = feadesdir(objfun, A, b, E, x0)
+    d, grad = feadesdir(objfun, A, b, E, x0)
     print("Feasible descent direction d:", d)
+    # lam = linesearch(objfun, A, b, x0, d)
+    # print("Optimal step size lambda:", lam)
+
+    # xstar, fstar, iterations = zoutendijk(objfun, A, b, E, e, x0)
+    # print("Optimal solution x*:", xstar)
+    # print("Optimal objective value f(x*):", fstar)
+    # print("Total iterations:", iterations)
