826d6d1f9ee5033408ce36c9f21f8b8b94ebd54a
ReFantasy <refantasy@ReFantasys-MacBook-Pro-M5.local>
2026-04-19 22:33:04 +0800

    update zoutendijk method

--- NumericalOptimization/constrained_optimization/zoutendijk.py
+++ NumericalOptimization/constrained_optimization/zoutendijk.py
@@ -1,16 +1,16 @@
-from math import isclose
-
 import jax
 import jax.numpy as jnp
 from mpax import create_lp, r2HPDHG
 from NumericalOptimization.utils import linear_search
 
 
-def feadesdir(objfun, A, b, E, xk):
+def feadesdir(objfun, A, b, E, xk, gradfun=None):
     """
     使用 JAX 和 MPAX 确定可行下降方向
     """
-    grad = jax.grad(objfun)(xk)
+    if gradfun == None:
+        gradfun = jax.grad(objfun)
+    grad_val = gradfun(xk)
 
     # 确定积极约束矩阵A1
     t = A @ xk
@@ -19,8 +19,8 @@ def feadesdir(objfun, A, b, E, xk):
     A_eq = E
 
     if A_ub.size == 0 and A_eq.size == 0:
-        d = -jnp.sign(grad)
-        return d, grad
+        d = -jnp.sign(grad_val)
+        return d, grad_val
 
     if A_ub.size == 0:
         A_ub = jnp.zeros(shape=(1, A.shape[1]))  # 没有不等式约束时，使用零矩阵
@@ -35,7 +35,7 @@ def feadesdir(objfun, A, b, E, xk):
     # s.t. Ax =  b
     #      Gx >= h
     lp = create_lp(
-        c=grad,
+        c=grad_val,
         A=A_eq,
         b=b_eq,
         G=-A_ub,
@@ -47,7 +47,7 @@ def feadesdir(objfun, A, b, E, xk):
     solver = r2HPDHG(verbose=False)
     result = solver.optimize(lp)
 
-    return result.primal_solution, grad
+    return result.primal_solution, grad_val
 
 
 def linesearch(objfun, A, b, xk, d):
@@ -73,21 +73,21 @@ def linesearch(objfun, A, b, xk, d):
         lambda_max = jnp.min(q_pos / p_pos)
         # 如果 lambda_max 出现异常值（例如没提取到任何项），提供一个回退
         if jnp.isnan(lambda_max) or jnp.isinf(lambda_max):
-            lambda_max = 10.0
+            lambda_max = 100.0
 
     # 设置一维搜索目标函数
     phi = lambda lam: objfun(xk + lam * d)
 
-    final_lambda, _, _ = linear_search.golden(phi, a=0.0, b=lambda_max, epsilon=0.00001)
+    final_lambda, _, _ = linear_search.golden(phi, a=0.0, b=lambda_max, epsilon=1e-8)
     return final_lambda
 
 
-def zoutendijk(objfun, A, b, E, e, x0, max_iter=1000):
+def zoutendijk(objfun, A, b, E, x0, gradfun=None, max_iter=1000):
     k = 0
     xk = x0
 
     while True:
-        d, grad = feadesdir(objfun, A, b, E, xk)
+        d, grad = feadesdir(objfun, A, b, E, xk, gradfun=gradfun)
 
         if jnp.isclose(grad @ d, 0.0):
             return xk, objfun(xk), k
@@ -107,19 +107,19 @@ if __name__ == "__main__":
     def objfun(x):
         return x[0] ** 2 + x[1] ** 2 - 2 * x[0] - 4 * x[1] + 6.0
 
+    @jax.jit
+    def gradfun(x):
+        return jnp.array([2 * x[0] - 2, 2 * x[1] - 4])
+
     A = jnp.array([[2.0, 1.0], [2.0, -1.0], [-1.0, 0.0], [0.0, -1.0]])
     b = jnp.array([6.0, 0.0, 0.0, 0.0])
     E = jnp.array([])  # 没有等式约束
     e = jnp.array([])  # 没有等式约束的右侧值
+
     # x0 = jnp.array([1.0, 4.0])
     x0 = jnp.array([0.4483, 4.0112])
 
-    # d, grad = feadesdir(objfun, A, b, E, x0)
-    # print("Feasible descent direction d:", d)
-    # lam = linesearch(objfun, A, b, x0, d)
-    # print("Optimal step size lambda:", lam)
-
-    xstar, fstar, iterations = zoutendijk(objfun, A, b, E, e, x0)
-    print("Optimal solution x:", xstar)
-    print("Optimal objective value f(x):", fstar)
-    print("Total iterations:", iterations)
+    xstar, fstar, iterations = zoutendijk(objfun, A, b, E, x0, gradfun=None)  # gradfun=None: use jax auto diff
+    print(f"Total iterations             : {iterations}")
+    print(f"Optimal solution x           : {xstar}")
+    print(f"Optimal objective value f(x) : {fstar}")
